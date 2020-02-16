7f57124790cc1995ded9fc7a8a9006cf116b1daf
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-02-16 21:48:43 +0200

    Reorganizing the back-end structure

--- Mean-Stack-Guide-BackEnd-Node/backend/app.js
+++ Mean-Stack-Guide-BackEnd-Node/backend/app.js
@@ -3,8 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-//Adding MongoDB
-const Post = require('./models/post');
+//Import routes
+const postsRoutes = require('./routes/posts');
 
 const app = express();
 
@@ -22,64 +22,6 @@ app.use((req,res,next)=>{
     next();
 });
 
-//POST request
-app.post("/api/posts", (req,res,next)=>{
-    const post = new Post({
-        title: req.body.title,
-        content: req.body.content
-    });
-    post.save().then(result => {
-        res.status(201).json({
-            message: "Post added" 
-        });
-    });
-});
-
-//GET request
-app.get('/api/posts', (req,res,next) => {
-    Post.find().then(docs=>{
-        res.status(200).json({
-            message: "Posts fetched",
-            posts: docs
-        });
-    });
-});
-
-app.get('/api/posts/:id', (req,res,next) => {
-    Post.findById(req.params.id).then(post=>{
-        if (post){
-            res.status(200).json(post);
-        } else {
-            res.status(404).json({
-                message : 'Post not found'
-            });
-        }
-    });
-}); 
-
-//PUT request
-app.put('/api/posts/:id', (req,res,next) => {
-    const post = new Post({
-        _id : req.body.id,
-        title : req.body.title,
-        content : req.body.content
-    });
-
-    Post.updateOne({_id : req.params.id}, post).then(()=>{
-        res.status(200).json({
-            message : 'Post updated'
-        });
-    });
-});
-
-//DELETE request
-app.delete('/api/posts/:id', (req,res,next) => {
-
-    Post.deleteOne({_id:req.params.id}).then(result =>{
-        console.log(result);
-    });
-
-    res.status(200).json({message:"Post deleted"});
-});
+app.use(postsRoutes);
 
 module.exports = app;
