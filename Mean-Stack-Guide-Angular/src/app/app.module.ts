42030484b84dbd62dd1f846bb41c5184cbb8f8a7
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-02-16 16:55:49 +0200

    Angular : Addding Routing

--- Mean-Stack-Guide-Angular/src/app/app.module.ts
+++ Mean-Stack-Guide-Angular/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule, MatEx
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
