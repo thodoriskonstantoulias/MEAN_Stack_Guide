16ee9a8f2581b85d94f3fc74bf783544fda2a5b3
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-02-21 22:07:07 +0200

    Angular : Finishing the login front end

--- Mean-Stack-Guide-Angular/src/app/auth/login/login.component.ts
+++ Mean-Stack-Guide-Angular/src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   templateUrl: './login.component.html',
@@ -8,4 +9,7 @@ export class LoginComponent {
 
   constructor() { }
 
+  onLogin(form:NgForm){
+      
+  }
 }
