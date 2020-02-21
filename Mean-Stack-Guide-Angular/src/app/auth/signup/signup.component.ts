cf8b94417d71ea0311b3c3ce980f83e510f74b0a
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-02-21 22:12:04 +0200

    Angular : Sign up component implementation front -end

---
+++ Mean-Stack-Guide-Angular/src/app/auth/signup/signup.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  templateUrl: './signup.component.html',
+  styleUrls : ['./signup.component.css']
+})
+export class SignupComponent {
+
+  constructor() { }
+
+  onRegister(form:NgForm){
+      
+  }
+}
