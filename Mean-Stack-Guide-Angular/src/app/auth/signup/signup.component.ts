70d012c5b3fabff6252cd51beeb610c42be151d0
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-02-22 12:30:08 +0200

    Angular : Created service for posting user to server

--- Mean-Stack-Guide-Angular/src/app/auth/signup/signup.component.ts
+++ Mean-Stack-Guide-Angular/src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   templateUrl: './signup.component.html',
@@ -7,9 +8,14 @@ import { NgForm } from '@angular/forms';
 })
 export class SignupComponent {
 
-  constructor() { }
+  constructor(private authService:AuthService) { }
 
   onRegister(form:NgForm){
-      
+
+    if (form.invalid){
+      return;
+    }
+
+    this.authService.createUser(form.value.email,form.value.password);
   }
 }
